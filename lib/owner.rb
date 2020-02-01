ac6da35ff672997abbb5fa0950dda9983e50b591
S. Makai Takori <makait87@gmail.com>
2020-02-01 00:21:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,73 @@
+require 'pry'
+
 class Owner
-  # code goes here
-end
+  
+  attr_reader :name, :species
+  attr_accessor :pets
+  
+  @@all = []
+  
+  def initialize(name)
+  @name = name
+  @species = "human"
+  @@all << self
+  @pets = {:cats => [], :dogs=> []}
+  end  
+  
+  def say_species
+     "I am a #{@species}."
+  end  
+ 
+  def self.all
+    @@all
+  end  
+  
+  def self.count
+    @@all.length
+  end  
+  
+  def self.reset_all
+    @@all.clear
+  end 
+   
+  def self.pets
+    @pets  
+  end
+    
+  def cats
+    Cat.all.select {|cat| cat.owner == self }
+  end  
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end 
+  
+  def buy_cat(name)
+    name = Cat.new(name, self)
+  end  
+  
+  def buy_dog(name)
+    name = Dog.new(name, self)
+  end  
+  
+  def walk_dogs
+    Dog.all.map {|dog| dog.mood = "happy"}
+  end  
+  
+  def feed_cats
+    Cat.all.map {|cat| cat.mood = "happy"}
+  end 
+  
+  def sell_pets
+      pets.each {|pet| pet.mood = "nervous"}
+      pets.clear
+    end
+  end
+  
+  
+
+
+
+
+
+
