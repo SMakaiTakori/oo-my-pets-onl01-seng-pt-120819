6acc6b3b372906bc88eeba2827190b42cb2db77a
S. Makai Takori <makait87@gmail.com>
2020-02-01 01:15:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,8 +59,11 @@ class Owner
   end 
   
   def sell_pets
-    Cat.all.each {|cat| cat.mood = "nervous" && cat.owner = nil}
-    Dog.all.each {|dog| dog.mood = "nervous" && dog.owner = nil}
+    # Cat.all.each {|cat| cat.mood = "nervous" && cat.owner = nil}
+    # Dog.all.each {|dog| dog.mood = "nervous" && dog.owner = nil}
+    
+    self.cats
+    
     
     end
   end
